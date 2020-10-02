bf25fd10272999740cf893db047a971300297e79
adilsoncardeal <acardeal@gmail.com>
2020-10-02 17:19:18 -0300

    bkp arquivos

---
+++ Uri1134.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Uri1134{
+    public static void main(String arg[]){
+        Scanner teclado = new Scanner(System.in);
+
+        int Al;
+        int Ga;
+        int Di;
+        int Fi;
+        int comb;
+
+        Al = 0;
+        Ga = 0;
+        Di = 0;
+                  
+
+        do {
+        comb = teclado.nextInt();
+
+        } while(Fi == 4);    
+
+    }
+}
